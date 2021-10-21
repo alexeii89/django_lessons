20a6fd7be13f045a98ef1a875d30b87cb782b22f
ALexeii <mr.white1989@rambler.ru>
2021-10-21 21:33:05 +0500

    lesson02

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,10 +3,59 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'главная'
+    }
+    return render(request, 'mainapp/index.html', context)
+
+
+links_menu = [
+    {'url': 'products', 'title_name': 'Все'},
+    {'url': 'products_home', 'title_name': 'Дом'},
+    {'url': 'products_office', 'title_name': 'Офис'},
+    {'url': 'products_modern', 'title_name': 'Модерн'},
+    {'url': 'products_classic', 'title_name': 'Классика'}
+]
+
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'контакты'
+    }
+    return render(request, 'mainapp/contact.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'links_menu': links_menu,
+        'title': 'все продукты'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'продукты для дома'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'продукты для офиса'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'продукты модерн'
+    }
+    return render(request, 'mainapp/products.html', context=context)
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'продукты классика'
+    }
+    return render(request, 'mainapp/products.html', context=context)
