981d62733e1b6b275bc15077802b233671fb08e9
Alex <alex@cppk.lc>
2021-11-01 12:32:09 +0500

    lesson05

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -4,7 +4,6 @@ from mainapp import views as mainapp
 app_name = "mainapp"
 
 urlpatterns = [
-    path('', mainapp.products, name='products'),
-    path('<int:pk>/', mainapp.products, name='category'),
-
+    path('', mainapp.products, name='index'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
+from random import random, shuffle
+from basketapp.models import Basket
+
 # Create your views here.
 
 
@@ -19,8 +23,55 @@ def contact(request):
 
 
 def products(request, pk=None):
+    print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by('price')
+
+    basket = []
+    sum_basket = 0
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        for i in basket:
+            sum_basket = sum_basket + i.quantity
+
+    if pk:
+        if pk == '0':
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(
+                category__pk=pk).order_by('price')
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+            'sum_basket': sum_basket
+        }
+
+        return render(request, 'mainapp/products_list.html', content)
+
+    same_products = list(Product.objects.all().order_by('pk')[:10])
+    shuffle(same_products)
+    print(same_products)
     context = {
-        'links_menu': ProductCategory.objects.all(),
-        'title': 'все продукты'
+        'title': title,
+        'links_menu': links_menu,
+        'first_same_products': same_products[0],
+        'same_products': same_products[1:4]
     }
-    return render(request, 'mainapp/products.html', context=context)
+
+    return render(request, 'mainapp/products.html', context)
