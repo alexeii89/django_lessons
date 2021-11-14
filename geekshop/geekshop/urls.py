e7f3b3cbb812e242cc43cb977b5605f807efdc84
ALexeii <mr.white1989@rambler.ru>
2021-11-14 16:27:40 +0500

    lesson07

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('products/', include('mainapp.urls', namespace='products')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('admin/', admin.site.urls),
+    path('adminapp/', include('adminapp.urls', namespace='adminapp')),
+    path('control/', admin.site.urls),
 
 ]
 
